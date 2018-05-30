723c1ede3320e148e618dfbba63d8d09a4c9c91f
Kuba <j.antoniak8@gmail.com>
2018-05-30 06:02:40 +0200

    fixed value function

--- src/main/scala/App.scala
+++ src/main/scala/App.scala
@@ -22,14 +22,15 @@ object App {
 
   }
 
-  def value(xy1 : (Int, Int), xy2 : (Int, Int)) : Double = Math.sqrt((xy2._2 - xy1._2) * (xy2._1 - xy1._1))
+  def value(xy1 : (Int, Int), xy2 : (Int, Int)) : Double =
+    Math.sqrt(Math.pow(xy2._2 - xy1._2, 2) + Math.pow(xy2._1 - xy1._1, 2))
 
   def aStar(graph : Graph[(Int, Int), Double], source : Node[(Int, Int)], destination : Node[(Int, Int)],
             cost : Double, heuristics : Double) : List[Node[(Int, Int)]]  = {
     if(source == destination) {
       return List(destination)
     }
-    val newNode = graph.edgeMap.apply(source).minBy(node => {cost + graph.getEdge(source, node)._3 + value(node.i, destination.i) })//min(node => )
+    val newNode = graph.edgeMap.apply(source).minBy(node => {cost + graph.getEdge(source, node)._3 + value(node.i, destination.i) })
     newNode :: aStar(graph, newNode, destination, cost + graph.getEdge(source, newNode)._3, value(newNode.i, destination.i))
   }
 }
