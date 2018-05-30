068fbd972cfc57cf5069e2b4f3c1e7447397e45f
Kuba <j.antoniak8@gmail.com>
2018-05-31 01:49:42 +0200

    improved A* algorithm

--- src/main/scala/App.scala
+++ src/main/scala/App.scala
@@ -4,7 +4,7 @@ import scala.math.Numeric._
 import scala.math.Numeric.Implicits._
 import Ordering.Implicits._
 
-class Town(val value : (Int, Int)) extends  Heuristics[(Int, Int), Double] {
+case class Town(value: (Int, Int), name: String) extends Ordered[Town] with  Heuristics[(Int, Int), Double] {
   override def heuristicValue(other: Heuristics[(Int, Int), Double]): Double =
     Math.sqrt(Math.pow(other.value._2 - value._2, 2) + Math.pow(other.value._1 - value._1, 2))
 
@@ -17,6 +17,15 @@ class Town(val value : (Int, Int)) extends  Heuristics[(Int, Int), Double] {
     val state = Seq(value)
     state.map(_.hashCode()).foldLeft(0)((a, b) => 31 * a + b)
   }
+
+  override def compareTo(that: Town): Int = {
+    if (value._1 > that.value._1 || (value._1 == that.value._1 && value._2 > that.value._2)) 1
+    else if (value._1 == that.value._1 && value._2 == that.value._2) 0
+    else -1
+  }
+
+  override def compare(that: Town): Int = compareTo(that)
+
 }
 
 
@@ -24,14 +33,17 @@ class Town(val value : (Int, Int)) extends  Heuristics[(Int, Int), Double] {
 object App {
   def main(args: Array[String]) {
     println("Hello basic-project!")
+//    val l1= List(0, 3, 6, 12, 14, 15, 16, 17)
+//    val l2 = l1.zip(l1.tail)
+//    l2.foreach(println)
     var graph = new Graph[(Int, Int), Double, Town](Set.empty,Set.empty )
-    graph = graph.addVertex(Node[Town](new Town(3, 4)))
-    graph = graph.addVertex(Node[Town](new Town(4, 4)))
-    graph = graph.addVertex(Node[Town](new Town(5, 4)))
-    graph = graph.addVertex(Node[Town](new Town(6, 4)))
-    graph = graph.addVertex(Node[Town](new Town(7, 4)))
-    graph = graph.addVertex(Node[Town](new Town(8, 4)))
-    graph = graph.addVertex(Node[Town](new Town(9, 4)))
+    graph = graph.addVertex(Node( Town((3, 4), "jeden")))
+    graph = graph.addVertex(Node[Town]( Town((4, 4), "dwa")))
+    graph = graph.addVertex(Node[Town]( Town((5, 4), "trzy")))
+    graph = graph.addVertex(Node[Town]( Town((6, 4), "cztery")))
+    graph = graph.addVertex(Node[Town]( Town((7, 4), "piec")))
+    graph = graph.addVertex(Node[Town]( Town((8, 4), "szesc")))
+    graph = graph.addVertex(Node[Town]( Town((9, 4), "siedem")))
     graph.vertices.foreach(v1 =>
       graph.vertices.foreach(v2 =>
         if((v1.value.value == (3, 4) && v2.value.value == (9, 4)) || (v1.value.value == (9, 4) && v2.value.value == (3, 4))) {
@@ -41,9 +53,9 @@ object App {
         graph = graph.addEdge(Edge(v1, v2, 4))
       })  )
 
-    val result = graph.aStar(Node(new Town(3,4)), Node(new Town(9,4)), 0)
-    result.foreach(vertex => println(vertex.value.value))
-
+    val result = graph.routeBetween(Town((3,4), "jeden"), Town((9,4), "siedem"))
+    println(result._1)
+    result._2.foreach(println)
   }
 
 
--- src/main/scala/Graph.scala
+++ src/main/scala/Graph.scala
@@ -13,7 +13,7 @@ case class Edge[T, V](node1: Node[T], node2: Node[T], weight: V) {
   def contains(node: Node[T]): Boolean = node1 == node || node2 == node
 }
 
-class Graph[T, V, N <: Heuristics[T,V]](val edges: Set[Edge[N,V]], val vertices : Set[Node[N]]) {
+class Graph[T, V: Ordering : Numeric, N <: Heuristics[T,V]](val edges: Set[Edge[N,V]], val vertices : Set[Node[N]]) {
 
   def addVertex(node: Node[N]) = new Graph[T, V, N](edges, vertices + node)
 
@@ -24,29 +24,74 @@ class Graph[T, V, N <: Heuristics[T,V]](val edges: Set[Edge[N,V]], val vertices 
 
   def removeEdge(edge: Edge[N, V]) = new Graph[T, V, N](edges.filter(_ != edge), vertices)
 
-  def getEdge(source : Node[N], destination : Node[N]) : Edge[N,V] = edges.filter(edge =>
-    edge.node1 == source && edge.node2== destination).head
+  def getEdge(source: Node[N], destination: Node[N]): Edge[N, V] = edges.filter(edge =>
+    edge.node1 == source && edge.node2 == destination).head
 
   lazy val edgeMap: Map[N, Set[Node[N]]] = edges.groupBy(_.node1.value).mapValues(s => s.map(_.node2))
 
-  private def sum(list: List[V])(implicit n: Numeric[V]): V = list.reduce((x: V, y: V) =>n.plus(x, y))
+  private def sum(list: List[V])(implicit n: Numeric[V]): V = list.reduce((x: V, y: V) => n.plus(x, y))
 
-  def aStar(sourceNode : Node[N], destination : Node[N], initialCost : V)(implicit n: Numeric[V]) : List[Node[N]]  = {
-    val initialHeuristics: V = sourceNode.value.heuristicValue(destination.value)
 
-    @tailrec
-    def iteration(source: Node[N], cost: V, heuristics: V, path: List[Node[N]]): List[Node[N]] = source match {
-      case src if src == destination => path ++ List(src)
-      case _ =>
-        val newNode = edgeMap.apply(source.value).minBy(node =>
-          sum(List(cost, getEdge(source, node).weight, node.value.heuristicValue(destination.value))))
-    iteration(
-          newNode,
-          sum(List(cost, getEdge(source, newNode).weight)),
-          newNode.value.heuristicValue(destination.value),
-          path ++ List(source))
+
+
+  def routeBetween(source: N, destination: N): (V, List[Node[N]]) = {
+
+    case class Route(alreadyVisited: List[Node[N]]) {
+      def cost(): V = {
+        val visitedEdges = alreadyVisited.zip(alreadyVisited.tail)
+        sum(alreadyVisited.last.value.heuristicValue(destination) ::
+          visitedEdges.map { case (n1, n2) => getEdge(n1, n2).weight })
+      }
     }
 
-    iteration(sourceNode, initialCost, initialHeuristics, Nil)
+    def beginJourney(source: N): Route = {
+      discoverNewRoutes(List(Route(List(Node[N](source)))))
+    }
+
+    @tailrec
+    def discoverNewRoutes(allRoutes: List[Route]): Route = {
+      val sortedByCost = allRoutes.sortBy(_.cost())
+      val cheapestRoute = sortedByCost.headOption.getOrElse(sys.error("An error occurred."))
+      val latestNode = cheapestRoute.alreadyVisited.last.value
+      latestNode match {
+        case `destination` => cheapestRoute
+        case cccMatched =>
+          val newRoutes = for (neighbourNode <- edgeMap(cccMatched).toList.filter(!cheapestRoute.alreadyVisited.contains(_)))
+            yield Route(cheapestRoute.alreadyVisited :+ neighbourNode)
+          discoverNewRoutes(newRoutes ++ sortedByCost.tail)
+      }
+    }
+
+    val route = beginJourney(source)
+    (route.cost(), route.alreadyVisited)
   }
 }
+
+
+//private def oddElements(arr: List[V]): List[V] = arr.indices.collect { case i if i % 2 == 0 => arr(i) }.toList
+
+
+//  def aStar(sourceNode : Node[N], destination : Node[N], initialCost : V)(implicit n: Numeric[V]) : List[Node[N]]  = {
+//
+//    val initialHeuristics: V = sourceNode.value.heuristicValue(destination.value)
+//
+//    val edgeSet : List[V] = oddElements(edges.toList.unzip3[Node[N], Node[N], V](edge =>
+//      (edge.node1, edge.node2, edge.weight))._3.sorted)
+//    edgeSet.foreach(println)
+//    val shortestPaths = edges.toList.sortBy(edge =>
+//      (edge.node1.value.heuristicValue(edge.node2.value), edge.node1, edge.node2)).take(2 * vertices.size)
+//    @tailrec
+//    def iteration(source: Node[N], cost: V, heuristics: V, path: List[Node[N]]): List[Node[N]] = source match {
+//      case src if src == destination => path ++ List(src)
+//      case _ =>
+//        val newNode = edgeMap.apply(source.value).minBy(node =>
+//          sum(List(cost, getEdge(source, node).weight, node.value.heuristicValue(destination.value))))
+//    iteration(
+//          newNode,
+//          sum(List(cost, getEdge(source, newNode).weight)),
+//          newNode.value.heuristicValue(destination.value),
+//          path ++ List(source))
+//    }
+//
+//    iteration(sourceNode, initialCost, initialHeuristics, Nil)
+//  }
