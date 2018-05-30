b467101fd353c8ae550116fad45fdf0c9afc717e
Kuba <j.antoniak8@gmail.com>
2018-05-30 05:26:41 +0200

    added A* implementation prototype (no generics, not sure if works at all)

--- src/main/scala/App.scala
+++ src/main/scala/App.scala
@@ -9,6 +9,24 @@ object App {
     graph = graph.removeVertex(Node[Int](3))
     graph.vertices.foreach(v => println(v.i))
     graph.edges.foreach(e => println(e._3))
+
+    val destination = Node[(Int, Int)](3, 3)
+    var source = Node[(Int, Int)](4, 4)
+
+    var cost = 0
+    var heuristics = value(source.i, destination.i)
+
+  }
+
+  def value(xy1 : (Int, Int), xy2 : (Int, Int)) : Double = Math.sqrt((xy2._2 - xy1._2) * (xy2._1 - xy1._1))
+
+  def aStar(graph : Graph[(Int, Int), Double], source : Node[(Int, Int)], destination : Node[(Int, Int)],
+            cost : Double, heuristics : Double) : List[Node[(Int, Int)]]  = {
+    if(source == destination) {
+      return List(destination)
+    }
+    val newNode = graph.edgeMap.apply(source).minBy(node => {cost + graph.getEdge(source, node)._3 + value(node.i, destination.i) })//min(node => )
+    newNode :: aStar(graph, newNode, destination, cost, heuristics)
   }
 }
 
--- src/main/scala/Graph.scala
+++ src/main/scala/Graph.scala
@@ -10,6 +10,8 @@ class Graph[T, V](val edges : Set[(Node[T], Node[T], V)], val vertices : Set[Nod
 
   def removeEdge(edge: (Node[T], Node[T])) = new Graph[T, V](edges.filter(e => e._1 != edge._1 || e._2 != edge._2), vertices)
 
+  def getEdge(source : Node[T], destination : Node[T]) : (Node[T], Node[T], V) = edges.filter(edge => edge._1 == source && edge._2 == destination).head
+
   lazy val edgeMap: Map[Node[T], Set[Node[T]]] = edges.groupBy(_._1).mapValues(s => s.map(_._2))
 
 }
