f4707070f0a0530d8e6f07a55e2bc4e3e206d503
Urbanov <dawidurbanowicz@outlook.com>
2018-05-30 21:03:16 +0200

    Changed class representing a city

--- src/main/scala/App.scala
+++ src/main/scala/App.scala
@@ -4,44 +4,28 @@ import scala.math.Numeric._
 import scala.math.Numeric.Implicits._
 import Ordering.Implicits._
 
-class Town(val value : (Int, Int)) extends  Heuristics[(Int, Int), Double] {
-  override def heuristicValue(other: Heuristics[(Int, Int), Double]): Double =
-    Math.sqrt(Math.pow(other.value._2 - value._2, 2) + Math.pow(other.value._1 - value._1, 2))
-
-  override def equals(obj: scala.Any): Boolean = obj match {
-    case town: Town => town.value == value
-    case _ => false
-  }
-
-  override def hashCode(): Int = {
-    val state = Seq(value)
-    state.map(_.hashCode()).foldLeft(0)((a, b) => 31 * a + b)
-  }
-}
-
-
 
 object App {
   def main(args: Array[String]) {
     println("Hello basic-project!")
-    var graph = new Graph[(Int, Int), Double, Town](Set.empty,Set.empty )
-    graph = graph.addVertex(Node[Town](new Town(3, 4)))
-    graph = graph.addVertex(Node[Town](new Town(4, 4)))
-    graph = graph.addVertex(Node[Town](new Town(5, 4)))
-    graph = graph.addVertex(Node[Town](new Town(6, 4)))
-    graph = graph.addVertex(Node[Town](new Town(7, 4)))
-    graph = graph.addVertex(Node[Town](new Town(8, 4)))
-    graph = graph.addVertex(Node[Town](new Town(9, 4)))
+    var graph = new Graph[(Int, Int), Double, City](Set.empty,Set.empty )
+    graph = graph.addVertex(Node[City](City((3, 4), "1")))
+    graph = graph.addVertex(Node[City](City((4, 4), "2")))
+    graph = graph.addVertex(Node[City](City((5, 4), "3")))
+    graph = graph.addVertex(Node[City](City((6, 4), "4")))
+    graph = graph.addVertex(Node[City](City((7, 4), "5")))
+    graph = graph.addVertex(Node[City](City((8, 4), "6")))
+    graph = graph.addVertex(Node[City](City((9, 4), "7")))
     graph.vertices.foreach(v1 =>
       graph.vertices.foreach(v2 =>
         if((v1.value.value == (3, 4) && v2.value.value == (9, 4)) || (v1.value.value == (9, 4) && v2.value.value == (3, 4))) {
           graph = graph.addEdge(Edge(v1, v2, 500))
         }
         else if(v1 != v2) {
-        graph = graph.addEdge(Edge(v1, v2, 4))
-      })  )
+          graph = graph.addEdge(Edge(v1, v2, 4))
+        })  )
 
-    val result = graph.aStar(Node(new Town(3,4)), Node(new Town(9,4)), 0)
+    val result = graph.aStar(Node[City](City((3, 4), "1")), Node[City](City((9, 4), "7")), 0)
     result.foreach(vertex => println(vertex.value.value))
 
   }
@@ -49,4 +33,3 @@ object App {
 
 
 }
-
--- src/main/scala/City.scala
+++ src/main/scala/City.scala
@@ -1,11 +1,14 @@
-//import math.sqrt
-//import math.pow
-//
-//class City(val x: Int, val y: Int, val name: String) extends Heuristics {
-//  override def heuristicValue(other: Heuristics): Double = {
-//    other match {
-//      case otherCity: City => sqrt(pow(x - otherCity.x, 2) + pow(y - otherCity.y, 2))
-//      case _ => throw new ClassCastException
-//    }
-//  }
-//}
+case class City(value: (Int, Int), name: String) extends  Heuristics[(Int, Int), Double] {
+  override def heuristicValue(other: Heuristics[(Int, Int), Double]): Double =
+  Math.sqrt(Math.pow(other.value._2 - value._2, 2) + Math.pow(other.value._1 - value._1, 2))
+
+  override def equals(obj: scala.Any): Boolean = obj match {
+    case town: City => town.value == value
+    case _ => false
+  }
+
+  override def hashCode(): Int = {
+    val state = Seq(value)
+    state.map(_.hashCode()).foldLeft(0)((a, b) => 31 * a + b)
+  }
+}
