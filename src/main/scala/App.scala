e5dbd1285c6c4874ffe2315136e7db24075fb8be
Kuba <j.antoniak8@gmail.com>
2018-05-30 03:34:24 +0200

    added removing vertex, adding edge and additional data structure for searching all neighbors of a particular vertex

--- src/main/scala/App.scala
+++ src/main/scala/App.scala
@@ -2,7 +2,13 @@
 object App {
   def main(args: Array[String]) {
     println("Hello basic-project!")
-
+    var graph = new Graph[Int, Double](Set.empty[(Node[Int], Node[Int], Double)],Set.empty[Node[Int]] )
+    graph = graph.addVertex(Node[Int](3))
+    graph = graph.addVertex(Node[Int](4))
+    graph = graph.addEdge((Node[Int](3), Node[Int](4), 5))
+    graph = graph.removeVertex(Node[Int](3))
+    graph.vertices.foreach(v => println(v.i))
+    graph.edges.foreach(e => println(e._3))
   }
 }
 
--- src/main/scala/Graph.scala
+++ src/main/scala/Graph.scala
@@ -1,8 +1,13 @@
 case class Node[T](i: T)
 
-class Graph[T, V](val edgeSet : Set[(Node[T], Node[T], V)], someNodes : Set[Node[T]]) {
+class Graph[T, V](val edges : Set[(Node[T], Node[T], V)], val vertices : Set[Node[T]]) {
 
-  def addVertex(node: Node[T]) = new Graph[T, V](edgeSet, someNodes + node)
+  def addVertex(node: Node[T]) = new Graph[T, V](edges, vertices + node)
 
+  def removeVertex(node: Node[T]) = new Graph[T,V](edges.filter(p => p._1 != node && p._2 != node), vertices - node)
+
+  def addEdge(edge: (Node[T], Node[T], V)) = new Graph[T, V](edges ++ Set(edge, (edge._2, edge._1, edge._3)), vertices + edge._1 + edge._2)
+
+  lazy val edgeMap: Map[Node[T], Set[Node[T]]] = edges.groupBy(_._1).mapValues(s => s.map(_._2))
 
 }
